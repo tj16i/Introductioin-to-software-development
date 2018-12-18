e1ba6ce71bb002a90d429257cd78753bb20ef2c4
tj16i <34540274+tj16i@users.noreply.github.com>
2018-12-18 14:03:25 +0000

    Add files via upload

---
+++ Week 13 (2).py
@@ -0,0 +1,13 @@
+class Animal():
+    def __init__(self, specie, name):
+        self.name = name
+        self.specie = specie
+class Mammal (Animal):
+    def __init__(self, name):
+        self.name = name
+class Insect (Animal):
+    def __init__(self, name):
+        self.name = name
+
+print(self.name = "Dog")
+print(self.name = "Bees")
